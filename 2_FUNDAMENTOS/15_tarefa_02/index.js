d400293594afdde756a9ee2c140193d273748d34
Yohanna Rodrigues <97924905+YohannaRodrigues@users.noreply.github.com>
2024-04-16 21:21:34 -0300

    feat: fundamentos aula 15

--- 2_FUNDAMENTOS/15_tarefa_02/index.js
+++ 2_FUNDAMENTOS/15_tarefa_02/index.js
@@ -1,15 +1,19 @@
-const inquirer = require('inquirer')
-const chalk = require('chalk')
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 inquirer
   .prompt([
-    { name: 'nome', message: 'Qual o seu nome?' },
-    { name: 'idade', message: 'Qual a sua idade?' },
+    { name: "nome", message: "Qual o seu nome?" },
+    { name: "idade", message: "Qual a sua idade?" },
   ])
   .then((answers) => {
-    const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade} anos`
-    console.log(chalk.bgYellow.black(response))
+    const response = `O nome do usuário é ${answers.nome} e ele tem ${answers.idade} anos`;
+    console.log(chalk.bgYellow.black(response));
   })
   .catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
+
+// O inquirer para gerar perguntas
+// O .then para sucesso ao pegar as respostas
+// O catch para erro ao pegar as respostas
